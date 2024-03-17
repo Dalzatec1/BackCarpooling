bc9b55f5cbb213d6f02bca57ae2d1dc7fab7bc9f
Henao07 <134786287+Henao07@users.noreply.github.com>
2024-03-17 12:30:22 -0500

    Add files via upload

--- Back-usuario/src/main/java/com/demosql/demo/services/ViajesService.java
+++ Back-usuario/src/main/java/com/demosql/demo/services/ViajesService.java
@@ -1,8 +1,28 @@
 package com.demosql.demo.services;
 
+import com.demosql.demo.models.Viaje;
+import com.demosql.demo.repositories.ViajeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+@Service
 public class ViajesService {
 
+    @Autowired
+    private ViajeRepository viajeRepository;
 
-    // listarviajes(){}
+    public Iterable<Viaje> getAllTripsForUser() {
+        return viajeRepository.findAll();
+    }
 
+    // Filter by departure point
+    public Viaje getTripsByDeparturePointContaining(String departure) {
+        return viajeRepository.findBypuntoPartida(departure);
+    }
+
+    // Filter by arrival point
+    public Viaje getTripsByArrivalPointContaining(String arrival) {
+        return viajeRepository.finBypuntoLlegada(arrival);
+    }
 }
